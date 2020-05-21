ef58cb507d0d93aa39d9c419dd39214b1d46c607
darksqualls1990 <62804632+darksqualls1990@users.noreply.github.com>
2020-05-20 22:30:17 -0500

    a

---
+++ cmrService/LoginUsuarioService.cs
@@ -0,0 +1,14 @@
+﻿using cmrModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmrService
+{
+    public interface LoginUsuarioService
+    {
+        LoginUsuario validarLogin(long id);
+    }
+}
